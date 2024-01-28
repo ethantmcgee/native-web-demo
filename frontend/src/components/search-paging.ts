87202d5835fad1ef930cedc5b13c48dc1629aa80
Ethan McGee <854614+ethantmcgee@users.noreply.github.com>
2024-01-27 21:04:13 -0500

    add total back

--- frontend/src/components/search-paging.ts
+++ frontend/src/components/search-paging.ts
@@ -77,10 +77,10 @@ export class SearchPaging extends LitElement {
         <div class="grid grid-cols-1">
           <div class="flex justify-center">
             <p class="text-sm">
-              Showing ${(this.page - 1) * this.perPage + 1} - ${this.page * this.perPage}
+              Showing ${(this.page - 1) * this.perPage + 1} - ${this.page * this.perPage} of ${this.total}
             <p>
           </div>
-          <div class="flex text-sm">
+          <div class="flex justify-center text-sm">
             <select id="sortBy" name="sortBy" class="mt-1 block rounded-md border-0 py-1 pl-1 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6" @change=${this.firePerPage}>
               ${this.getOptions()}
             </select> <span class="mt-2 ml-1">per page</span>
